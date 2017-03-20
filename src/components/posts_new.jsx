ff02e8c42e241443f3708124d0100cad96eacfef
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-20 14:03:15 -0700

    fixed form

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -10,20 +10,28 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
-          <div className="text-help">{title.touched ? title.error : ''}</div>
+          <div className="form-control-feedback">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="form-control-feedback">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content}/>
+          <div className="form-control-feedback">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -37,6 +45,14 @@ function validate(values) {
 
   if (!values.title) {
     errors.title = 'Enter a username';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter a content';
   }
 
   return errors;
