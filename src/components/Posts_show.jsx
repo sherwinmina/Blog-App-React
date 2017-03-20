c6badf89dd122a0fccb00484dfb9b97a4607b91f
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-20 16:28:12 -0700

    added delete post

---
+++ src/components/Posts_show.jsx
@@ -0,0 +1,47 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.fetchPost(this.props.params.id)
+      .then(() => { this.context.router.push('/'); });
+  }
+
+  render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/"> Back To Index</Link>
+        <button
+          onClick={this.onDeleteClick.bind(this)}
+          className="btn btn-danger pull-right">
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {post: state.posts.post};
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow)
