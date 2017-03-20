caf0f8887b3ee8055062823d036e24398af9b124
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-20 11:55:32 -0700

    started Form View

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const ROOT_URL = 'http://reduxblog.heroku.com/api';
+export const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=feasdasda';
 
 export function fetchPosts() {
